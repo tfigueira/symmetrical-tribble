4d8aaa2479b9865585804c7eaffa46bdecb8a647
Thiago Figueira <tfigueira@gmail.com>
2025-04-06 13:39:06 -0300

    Novas atualizações

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,16 @@
-import React, { useEffect,useState } from 'react';
 import { BrowserRouter } from "react-router";
 import Rotas from './routes/routes';
-import CadastroAluno from './pages/cadastro';
-import ListarAlunos from './pages/listarAlunos';
-import { Button } from '@mui/material';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <Rotas/>
-//     </BrowserRouter>
-//   );
-// }
+import { CallMenu } from "./Components/Menu";
 
 function App() {
-
-  const [page, setPage] = useState<string>('CadastroAluno');
-
-  const currentPath = window.location.pathname;
-
-  useEffect(() => {
-    console.log(currentPath);
-  }, [currentPath]);
-
   return (
     <div>
-      {/* <cadastroAluno/> */}
-      <Button onClick={()=> setPage('CadastroAluno')}>Cadastro de Alunos</Button>
-      <Button onClick={()=> setPage('listarAlunos')}>Listar Alunos</Button>
-      <Button>Home</Button>
-      {
-        currentPath == "/CadastroAluno" &&
-        //page == "CadastroAluno" &&
-        <div>
-          <CadastroAluno/>
-        </div>
-      }
-
-      {
-        currentPath == "/listarAlunos" &&
-        //page == "listaralunos" &&
-        <div>
-          <ListarAlunos/>
-        </div>
-      }
+     
+      <BrowserRouter>
+        <CallMenu />
+        <Rotas />
+      </BrowserRouter>
+          
     </div>
   );
 }
